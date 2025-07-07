731e32e95f32cb0c678b21244d43b23c742ef006
umair <umair391220@gmail.com>
2025-07-07 16:49:09 +0400

    State Management

---
+++ user_auth_system/frontend/src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+
+export const store = configureStore ({
+    reducer: {
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+
+
